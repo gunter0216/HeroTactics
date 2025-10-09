535a32951893b5b986ecd596f3ba418eee1ebb8e
Nikita Ermolaev <Gunter0216@yandex.ru>
2025-10-09 21:12:01 +0300

    art

---
+++ Assets/App/Core/BattleField/External/Path/IPathFinder.cs
@@ -0,0 +1,14 @@
+﻿// using System.Collections.Generic;
+// using App.Game.DungeonGenerator.Runtime.Utility;
+// using DungeonGenerator.Matrices;
+//
+// namespace App.Game.DungeonGenerator.Runtime.PathFinders
+// {
+//     public interface IPathFinder
+//     {
+//         Optional<List<Position>> FindPath(
+//             Matrix inputMatrix,
+//             Position from,
+//             Position to);
+//     }
+// }
